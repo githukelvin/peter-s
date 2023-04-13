2f6addfabde8e1f9a251925df3ad37f41cc22b3c
githukelvin <kelvingithu019@gmail.com>
2023-04-13 17:42:46 +0300

    build ending

--- minJs/main.js
+++ minJs/main.js
@@ -1,2 +1,2 @@
-
+let btn=document.querySelector(".btn"),ham=document.querySelector(".ham");btn.addEventListener("click",(()=>{window.scrollTo({top:0,behavior:"smooth"})}));
 //# sourceMappingURL=main.js.map
